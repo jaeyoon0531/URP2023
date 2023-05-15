a5055aa655f5423d023bb6dd84b7e8d6c8682de7
jaeyoon0531 <97937116+jaeyoon0531@users.noreply.github.com>
2023-05-15 16:14:23 +0900

    Create TX_TRANS_LAYER.sv

---
+++ RTL/TX_TRANS_LAYER.sv
@@ -0,0 +1,41 @@
+// Create Date: 2023/05/08 16:54:14
+// Design Name: 
+// Module Name: TRANSACTION
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module TX_TRANS_LAYER (
+  input wire clk,
+  input wire reset_n,
+  
+  //software interface
+  AXI_W_IF.DST        axi_w_if,
+  AXI_A_IF.DST        axi_aw_if,
+  AXI_A_IF.DST        axi_ar_if,
+  
+  //data link layer interface
+  output reg [63:0] tlp_out,
+  output reg tlp_out_valid,
+  input  reg tlp_in_ready
+  
+);
+
+
+
+    /*
+    * FILL YOUR CODES HERE
+    */
+
+  
+  
+endmodule
