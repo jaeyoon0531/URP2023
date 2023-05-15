ed0da4c583e8818a8d01f39abd7378840be38427
jaeyoon0531 <97937116+jaeyoon0531@users.noreply.github.com>
2023-05-15 17:51:43 +0900

    Update TX_DATA_LINK_LAYER.sv

--- RTL/TX_DATA_LINK_LAYER.sv
+++ RTL/TX_DATA_LINK_LAYER.sv
@@ -21,20 +21,20 @@
 
 
 module TX_DATA_LINK_LAYER (
-    input wire clk,
-    input wire reset_n,
+    input wire                  clk,
+    input wire                  reset_n,
     
     // Transaction Layer interface
-    input wire [31:0] tlp_data_in,
-    input wire tlp_data_in_valid,
-    output wire tlp_data_in_ready,
+    input wire [31:0]           tlp_data_in,
+    input wire                  tlp_data_in_valid,
+    output wire                 tlp_data_in_ready,
     
     // Physical Layer interface
-    output wire [47:0] tlp_data_out,
-    output wire tlp_data_out_valid,
-    input wire tlp_data_out_ready,
-    input wire ack,
-    input wire nack
+    output wire [47:0]          tlp_data_out,
+    output wire                 tlp_data_out_valid,
+    input wire                  tlp_data_out_ready,
+    input wire                  ack,
+    input wire                  nack
 );
 
     
