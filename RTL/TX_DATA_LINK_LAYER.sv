473ab76453c67535facd1e1084f4642f7a20460d
jaeyoon0531 <97937116+jaeyoon0531@users.noreply.github.com>
2023-05-17 01:57:03 +0900

    Update TX_DATA_LINK_LAYER.sv

--- RTL/TX_DATA_LINK_LAYER.sv
+++ RTL/TX_DATA_LINK_LAYER.sv
@@ -21,20 +21,20 @@
 
 
 module TX_DATA_LINK_LAYER (
-    input wire                  clk,
-    input wire                  reset_n,
+    input   wire                      clk,
+    input   wire                      reset_n,
     
     // Transaction Layer interface
-    input wire [31:0]           tlp_data_in,
-    input wire                  tlp_data_in_valid,
-    output wire                 tlp_data_in_ready,
+    input   wire [1023:0]             tlp_data_in,
+    input   wire                      tlp_data_in_valid,
+    output  reg                       tlp_data_out_ready,
     
     // Physical Layer interface
-    output wire [47:0]          tlp_data_out,
-    output wire                 tlp_data_out_valid,
-    input wire                  tlp_data_out_ready,
-    input wire                  ack,
-    input wire                  nack
+    output  reg  [1023:0]             tlp_data_out,
+    output  reg                       tlp_data_out_valid,
+    input   wire                      tlp_data_in_ready,
+    input   wire [31:0]               dllp
+      
 );
 
     
@@ -43,6 +43,8 @@ module TX_DATA_LINK_LAYER (
     */
     // LCRC 생성 / sequence number 생성 / Retry buffer
 
-   
+    
+    
+    
 
 endmodule
